35a1568477f7d0c11773da1adc2d70f8ad527ae6
fengjinqi <tarena_feng@126.com>
2019-11-01 18:37:21 +0800

    jdsdk

--- apps/jd_app/views.py
+++ apps/jd_app/views.py
@@ -15,7 +15,7 @@ def getRush(request):
     :return:
     """
     n=jd.api.UnionOpenGoodsJingfenQueryRequest('https://router.jd.com/api')
-    n.set_app_info(jd.appinfo('dd0e33fbd25eaab95f50cfebc72d8925','51400be7286e49f4aae8c37b64a7e443'))
+    n.set_app_info(jd.appinfo())
     n.eliteId = 22
     n.pageIndex = 1
     n.pageSize = 20
